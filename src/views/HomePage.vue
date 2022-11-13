a6e9c0435f38d53bc8c7a2ebc6e8ad8aeb3ff901
Rene Dekart <gorbachev-art@yandex.ru>
2022-11-13 23:03:32 +0300

    wip

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,14 +9,21 @@
       <router-link :to="{ name: 'dashboard' }"> клац </router-link>
     </div>
     <div>
-      Репозиторий можно найти здесь: 
-      <a href='https://github.com/ReneDekart/test_task_vue.git' target="_blank" > клац </a>
+      Репозиторий можно найти здесь:
+      <a href="https://github.com/ReneDekart/test_task_vue.git" target="_blank">
+        клац
+      </a>
+    </div>
+    <div>
+      Ссылка на github-pages:
+      <a href="https://renedekart.github.io/test_task_vue/" target="_blank">
+        enjoy
+      </a>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "home-page",
 };
@@ -27,13 +34,16 @@ export default {
 .home-page--root {
   text-align: center;
 
+  a {
+    font-weight: bold;
+    color: $c_blue;
+  }
+  > div{
+    margin-bottom: 30px;
+  }
+
   > div:nth-child(2) {
     margin-top: 50px;
-
-    > a {
-      font-weight: bold;
-      color: $c_blue;
-    }
   }
 }
 </style>
